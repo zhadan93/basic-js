0291efeeb0e483bff81520cca59746b3a3c393e4
zhadan93 <zhadan.evgenya@yandex.ru>
2021-03-19 18:17:43 +0300

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw new Error('');
+
+  let mas = [];
+  let controlSequences = {
+    '--discard-next': 0,
+    '--discard-prev': 1,
+    '--double-next': 2,
+    '--double-prev': 3,
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] in controlSequences) {
+
+      if (controlSequences[arr[i]] % 2 && i == 0 ||
+      !(controlSequences[arr[i]] % 2) && i == arr.length - 1) continue;
+
+      if (controlSequences[arr[i]] == 0) {
+        controlSequences[arr[i + 2]] % 2 ? i += 2 : i++;
+      } else if (controlSequences[arr[i]] == 1) {
+        mas.pop();
+      } else if (controlSequences[arr[i]] == 2) {
+        mas.push(arr[i + 1]);
+      } else if (controlSequences[arr[i]] == 3) {
+        mas.push(mas[mas.length - 1]);
+      }
+
+    } else {
+      mas.push(arr[i]);
+    }
+
+  }
+
+  return mas;
+}
