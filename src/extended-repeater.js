0806b38ec25c11eedca60d3a037632b58276f0d9
zhadan93 <zhadan.evgenya@yandex.ru>
2021-03-19 18:22:42 +0300

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'}) {
+  let res = [];
+  let substring = [];
+
+  if (typeof str != 'string') str = String(str);
+  if (typeof addition != 'string') addition = String(addition);
+
+  for (let i = 0; i < repeatTimes; i++) {
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      substring.push(addition);
+    }
+    substring = substring ? str + substring.join(additionSeparator) : str;
+    res.push(substring);
+    substring = [];
+  }
+  return res.join(separator);
 };
-  
+
