ae6f10cd0349d591d445db217418d099d7056bd0
zhadan93 <zhadan.evgenya@yandex.ru>
2021-03-19 18:19:48 +0300

    feat: implement simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  'linkMas': [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.linkMas.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    value = value !== undefined ? value : '';
+    this.linkMas.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(position) != 'number' || Math.trunc(position) < position
+    || position < 1 || position >= this.getLength()) {
+      this.linkMas = [];
+      throw new Error("");
+    }
+    this.linkMas.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.linkMas.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.linkMas.join('~~');
+    this.linkMas = [];
+    return res;
   }
 };
 
 module.exports = chainMaker;
+
